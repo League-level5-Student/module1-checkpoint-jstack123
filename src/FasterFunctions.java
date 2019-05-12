acb02ea13cf42ac40aad8c81cfbba126fbe8c95e
League Student <league@iMac-8.attlocal.net>
2019-05-12 16:52:36 -0700

    finish module 1 checkpoint level5

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,15 +1,53 @@
 
 public class FasterFunctions {
 	public static void main(String[] args) {
-		float totalTime = 0;
-		long startTime = System.currentTimeMillis();
+//		float totalTime = 0;
+//		long startTime = System.currentTimeMillis();
 		//Your Code Here
 		
+		Thread sort = new Thread(()-> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting slowSortLargeArray()...");
+			SlowFunctions.slowSortLargeArray();
+			System.out.println("slowSortLargeArray() Complete.");
+//			totalTime += endTime;
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("slowSortLargeArray() Time: " + endTime + "\n\n");
+		});
 		
+		Thread ackermann = new Thread(()-> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting ackermann(3, 14)...");
+			System.out.println("ackermann(3, 14): " + SlowFunctions.ackermann(3, 14));
+			System.out.println("ackermann(3, 14) Complete.");
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+//			totalTime += endTime;
+			System.out.println("ackerman(3, 14) Time: " + endTime + "\n\n");
+			float totalTime = endTime;
+			System.out.println("Total Time: " + totalTime + "\n\n");
+		});
 		
+		Thread sqrt = new Thread(() -> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting millionsOfSqrts()...");
+			SlowFunctions.millionsOfSqrts();
+			System.out.println("millionsOfSqrts() Complete.");
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+//			totalTime += endTime;
+			System.out.println("millionsOfSqrts()) Time: " + endTime + "\n\n");
+			
+		});
 		
-		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
-		totalTime += endTime;
-		System.out.println("Total Time: " + totalTime);
+		sort.start();
+		ackermann.start();
+		sqrt.start();
+		
+//		
+//		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+//		totalTime += endTime;
+//		System.out.println("Total Time: " + totalTime);
 	}
 }
--- src/SlowFunctions.java
+++ src/SlowFunctions.java
@@ -20,30 +20,47 @@ import java.util.Random;
 
 public class SlowFunctions {
 	public static void main(String[] args) {
-		float totalTime = 0;
-		long startTime = System.currentTimeMillis();
-		System.out.println("Starting slowSortLargeArray()...");
-		SlowFunctions.slowSortLargeArray();
-		System.out.println("slowSortLargeArray() Complete.");
-		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
-		totalTime += endTime;
-		System.out.println("slowSortLargeArray() Time: " + endTime + "\n\n");
+		Thread sort = new Thread(()-> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting slowSortLargeArray()...");
+			SlowFunctions.slowSortLargeArray();
+			System.out.println("slowSortLargeArray() Complete.");
+//			totalTime += endTime;
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("slowSortLargeArray() Time: " + endTime + "\n\n");
+		});
 		
-		startTime = System.currentTimeMillis();
-		System.out.println("Starting ackermann(3, 14)...");
-		System.out.println("ackermann(3, 14): " + SlowFunctions.ackermann(3, 14));
-		System.out.println("ackermann(3, 14) Complete.");
-		endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
-		totalTime += endTime;
-		System.out.println("ackerman(3, 14) Time: " + endTime + "\n\n");
+		Thread ackermann = new Thread(()-> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting ackermann(3, 14)...");
+			System.out.println("ackermann(3, 14): " + SlowFunctions.ackermann(3, 14));
+			System.out.println("ackermann(3, 14) Complete.");
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+//			totalTime += endTime;
+			System.out.println("ackerman(3, 14) Time: " + endTime + "\n\n");
+			float totalTime = endTime;
+			System.out.println("Total Time: " + totalTime + "\n\n");
+		});
 		
-		System.out.println("Starting millionsOfSqrts()...");
-		SlowFunctions.millionsOfSqrts();
-		System.out.println("millionsOfSqrts() Complete.");
-		endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
-		totalTime += endTime;
+		Thread sqrt = new Thread(() -> {
+			long startTime = System.currentTimeMillis();
+			float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+			System.out.println("Starting millionsOfSqrts()...");
+			SlowFunctions.millionsOfSqrts();
+			System.out.println("millionsOfSqrts() Complete.");
+			endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+//			totalTime += endTime;
+			System.out.println("millionsOfSqrts()) Time: " + endTime + "\n\n");
+			
+		});
 		
-		System.out.println("Total Time: " + totalTime + "\n\n");
+		sort.start();
+		ackermann.start();
+		sqrt.start();
+	
+		
 	}
 	
 	public static void slowSortLargeArray() {
